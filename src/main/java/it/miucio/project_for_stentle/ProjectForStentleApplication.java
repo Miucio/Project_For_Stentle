7c3ac9320f96e1e41010eb3a117cb72ddde38f85
miucio <lapennafederico@gmail.com>
2022-09-22 10:56:13 +0200

    First commit

---
+++ src/main/java/it/miucio/project_for_stentle/ProjectForStentleApplication.java
@@ -0,0 +1,13 @@
+package it.miucio.project_for_stentle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForStentleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectForStentleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/miucio/project_for_stentle/ProjectForStentleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.miucio.project_for_stentle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectForStentleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
